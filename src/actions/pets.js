58667a82190b62920175025955c965fdcfae9dd6
Shane Lupton <slupton89@gmail.com>
2018-11-16 11:51:55 -0700

    added reducer

--- src/actions/pets.js
+++ src/actions/pets.js
@@ -1,29 +1,42 @@
 const API_URI = 'http://localhost:3000/api/';
 
 export const FETCH_PET_REQUEST = 'FETCH_PET_REQUEST';
-function fetchPetRequest(petType) {
-  return {type: 'FETCH_PET_REQUEST', petType}
-}
+export const fetchPetRequest =(petType) => ({
+  type: 'FETCH_PET_REQUEST',
+  petType
+});
 
 export const FETCH_PET_SUCCESS = 'FETCH_PET_SUCCESS';
-function fetchPetSuccess(pet) {
-  return {type: 'FETCH_PET_SUCCESS', pet}
-}
+export const fetchPetSuccess = (petType ,pet) => ({
+  type: 'FETCH_PET_SUCCESS',
+  pet
+});
 
 export const FETCH_PET_FAILURE = 'FETCH_PET_FAILURE';
-function fetchPetFailure() {
-  return {type: 'FETCH_PET_FAILURE'}
-}
+export const fetchPetFailure = () => ({
+  type: 'FETCH_PET_FAILURE'
+});
 
 export const fetchPet = (petType) => (dispatch) => {
   dispatch(fetchPetRequest());
 
-  fetch(API_URI + petType)
+  return fetch(API_URI + petType, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
     .then(res => {
+      if(!res.ok) {
+        return Promise.reject({
+          message: res.statusText,
+          code: res.status
+        });
+      }
       return res.json();
     })
     .then(res => {
-      dispatch(fetchPetSuccess(res));
+      dispatch(fetchPetSuccess(petType, res));
     })
     .catch(err => {
       fetchPetFailure(err);
---
+++ src/reducers/pet-reducer.js
@@ -0,0 +1,39 @@
+import {
+  FETCH_PET_REQUEST,
+  FETCH_PET_SUCCESS,
+  FETCH_PET_FAILURE,
+} from '../actions/pets';
+
+const initialState = {
+  loading: false,
+  catToAdopt: null,
+  dogToAdopt: null
+}
+
+export const petReducer = (state = initialState, action) => {
+  if(action.type === FETCH_PET_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true
+    })
+  } else if(action.type === FETCH_PET_SUCCESS) {
+    if(action.petType === 'cat') {
+      return Object.assign({}, state, {
+        loading: false,
+        catToAdopt: action.pet,
+        error: null
+      });
+    } else if(action.petType === 'dog') {
+      return Object.assign({}, state, {
+        loading: false,
+        dogToAdopt: action.pet,
+        error: null
+      });
+    }
+  } else if(action.type === FETCH_PET_FAILURE) {
+    return Object.assign({}, state, {
+      loading: false,
+      error: action.error
+    });
+  }
+
+}
